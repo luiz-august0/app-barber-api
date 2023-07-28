8c5fcac037a599454ae55e0bfdbeb6210e153106
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-07-27 21:02:59 -0300

    Alterada chave USER do env pois estava ocorrendo erro na aws

--- api/config/mysql.js
+++ api/config/mysql.js
@@ -2,7 +2,7 @@ import * as mysql from 'mysql2';
 require('dotenv').config();
 
 var pool = mysql.createPool({
-    "user" : process.env.USER,
+    "user" : process.env.USUARIO,
     "password" : process.env.PASSWORD,
     "database" : process.env.DATABASE,
     "host" : process.env.HOST,
