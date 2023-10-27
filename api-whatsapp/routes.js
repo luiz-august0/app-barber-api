1ce50cf8812f83234a465d8ca9c427a112b51e59
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-10-27 11:14:24 -0300

    Separando api do whatsapp em outro repositório

--- api-whatsapp/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import WPController from "./routes/WPController";
-
-const routes = new Router();
-
-routes.post('/barberwp/mensagem', WPController.sendMessage);
-
-export default routes;
--- api-whatsapp/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-require('dotenv').config();
-
-app.listen(6000);
